0afae69aebf0f78d6fee6265967f743f05704f36
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-09-25 08:21:11 +0200

    Adding project.

---
+++ src/main/java/com/soprasteria/hackaton/teagile/TeAgileHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.hackaton.teagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeAgileHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeAgileHackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soprasteria/hackaton/teagile/TeAgileHackatonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soprasteria.hackaton.teagile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeAgileHackatonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
