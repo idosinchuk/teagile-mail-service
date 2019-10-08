b063af10d4c7f064b6ed75be1bd0a39b0295b455
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-08 15:58:15 +0200

    Deleted unused annotation

--- src/main/java/com/soprasteria/hackaton/teagile/mail/service/MailApplication.java
+++ src/main/java/com/soprasteria/hackaton/teagile/mail/service/MailApplication.java
@@ -2,9 +2,7 @@ package com.soprasteria.hackaton.teagile.mail.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
 public class MailApplication {
 
