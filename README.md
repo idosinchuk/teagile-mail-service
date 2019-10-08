574128263a9789da09545986df85593e068ec99a
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-08 11:14:26 +0200

    Added RabbitMQ and MailSender

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# teagile-core-service
-TEAgile Core Service for SopraSteria Hackaton
+# teagile-mail-service
+TEAgile Mail Service for SopraSteria Hackaton
+
+Install RabbitMQ: https://www.rabbitmq.com/install-windows.html#installer
+				  https://www.youtube.com/watch?v=oRIF1xKEI0I
+				  
+To run the rabbitmq server: 
+
+- cd C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.0
+- cd sbin
+- rabbitmq-service.bat start / rabbitmq-service.bat stop
+- rabbitmqctl status
+
+To enable rabbitmq interface:
+
+- cd C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.0
+- cd sbin 
+- rabbitmq-plugins enable rabbitmq_management
+- go to: localhost:15672
+- login with: 
+	user: guest
+	password: guest
--- src/main/java/com/soprasteria/hackaton/teagile/mail/service/mail/MailClient.java
+++ src/main/java/com/soprasteria/hackaton/teagile/mail/service/mail/MailClient.java
@@ -12,27 +12,27 @@ import com.soprasteria.hackaton.teagile.mail.service.dto.MailDTO;
 @Service
 public class MailClient {
 
-	private JavaMailSender mailSender;
-	
+	private JavaMailSender javaMailSender;
 	private MailContentBuilder mailContentBuilder;
 
 	@Autowired
 	public MailClient(JavaMailSender mailSender, MailContentBuilder mailContentBuilder) {
-		this.mailSender = mailSender;
+		this.javaMailSender = mailSender;
 		this.mailContentBuilder = mailContentBuilder;
 	}
 
 	public void prepareAndSend(MailDTO mailDTO, String type) {
 		MimeMessagePreparator messagePreparator = mimeMessage -> {
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-			messageHelper.setFrom("igor.dosinchuk@soprasteria.com");
+			messageHelper.setFrom("teagilehackaton@gmail.com");
 			messageHelper.setTo(mailDTO.getEmail());
 			messageHelper.setSubject("Welcome to TEAgile");
 			String content = mailContentBuilder.build("Congratulations, your account has been created successfully.");
 			messageHelper.setText(content, true);
 		};
+		
 		try {
-			mailSender.send(messagePreparator);
+			javaMailSender.send(messagePreparator);
 		} catch (MailException e) {
 			// runtime exception; compiler will not force you to handle it
 		}
